eba8d0268254a75412e129a6e038d00e7ac38479
Michu6 <karenmvaldivia@gmail.com>
2016-06-09 18:07:24 -0500

    close on click

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 		var $anchor = $(this);
 		$('html, body').stop().animate({
 		scrollTop: $($anchor.attr('href')).offset().top}, 2000, 'easeInOutExpo');
+		$("#sidenav-overlay").trigger("click");
 		event.preventDefault();
 	});
 	
